38606694904397e5455c9255e59eccb28e9a5c73
Jeswin Kumar <jeswinpk@agilehead.com>
2018-08-16 10:54:19 +0530

    Fix npm link

--- npmlink.sh
+++ npmlink.sh
@@ -1,5 +1,13 @@
-npm link ../scuttlespace-service-user-graphql-schema
+set -e 
+BASHO_VERSION=$(basho -v | basho 'x.split(".")[2]' -j 'parseInt(x) >= 43 && "OK"')
 
-pushd ../scuttlespace-service-user
-./npmlink.sh
-popd
+if [[ $BASHO_VERSION == "OK" ]]
+then
+  (
+    echo scuttlespace-service-user-graphql-schema &&
+    echo scuttlespace-service-user
+  ) | basho -n proj -e 'cd ../${x} && npm link && [ -f npmlink.sh ] && ./npmlink.sh || echo skipped' \
+        -s proj -j '`update ${x}`'
+else
+  echo "Install basho version 0.0.43 or higher."
+fi
